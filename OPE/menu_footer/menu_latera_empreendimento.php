c773ba3eed1804e64dafc0eb2ad6b49472662739
LeonardoBernardes <lbernardes.silva@outlook.com>
2018-10-18 02:07:19 -0300

    mrnr

--- OPE/menu_footer/menu_latera_empreendimento.php
+++ OPE/menu_footer/menu_latera_empreendimento.php
@@ -6,6 +6,7 @@
 body {
     font-family: "Lato", sans-serif;
 }
+/*
 #navbar {
   overflow: hidden;
   background-color: #333;
@@ -15,23 +16,24 @@ body {
   float: left;
   display: block;
   color: #f2f2f2;
+  color:rgb(0, 0, 0) !important;
   text-align: center;
   padding: 14px 16px;
   text-decoration: none;
   font-size: 17px;
-}
-
+}*/
+/*
 #navbar a:hover {
   background-color: #ddd;
   color: black;
-}
-
+}*/
+/*
 #navbar a.active {
   background-color: #4CAF50;
   color: white;
 }
 
-
+*/
 .sidenav {
     width: 180px;
     height:100%;
@@ -40,18 +42,19 @@ body {
     top: 10px;
     background: #eee;
     overflow-x: hidden;
-    padding: 8px 0;
+    padding: 0px;
 }
 
 .sidenav a {
-    padding: 10px 8px 30px 16px;
+   
     text-decoration: none;
-    color: #2196F3;
+   /* color: #2196F3;*/
+    color:rgb(0, 0, 0) !important;
     display: block;
 }
 
 .sidenav a:hover {
-    color: #064579;
+    color: white;
 }
 
 .main {
@@ -59,7 +62,7 @@ body {
     font-size: 20px; /* Increased text to enable scrolling */
     padding: 10px;
 }
-
+/*
 .sticky {
   position: fixed;
   top: 0;
@@ -68,16 +71,35 @@ body {
 
 .sticky + .content {
   padding-top: 60px;
+}*/
+.nav-pills >li{
+    margin-left:5px;
+    margin-right:5px;
 }
 
 @media screen and (max-height: 450px) {
     .sidenav {padding-top: 15px;}
     .sidenav a {font-size: 18px;}
 }
-
+.nav-pills >li>a{
+    border-radius:10px;
+   
+}
+.nav-pills>li.active>a,.nav-pills>li.active>a:focus,.nav-pills>li.active>a:hover
+    {
+        color:#fff;
+        background-color:#8bff8e;
+        padding: 15px;
+     
+    }
 #menu_lateral{
     background: #28a745;
-
+    box-shadow: 3px 0px black;
+    color:rgb(0, 0, 0) !important;
+    line-height: 4.5;
+}
+ul{
+    width:100%;
 }
 </style>
 
@@ -114,7 +136,7 @@ if ($_SESSION['grup_id'] == 4){
 
         if(!empty($row2)){
 
-            $menu .='   <li class="nav-item">
+            $menu .='   <li class="nav-item active">
                             <a class="nav-link" href="../animais/consulta_animais.php"><img src="../static/icones/animais.png" style="width:20px;"/> Meus Animais </a>
                         </li>
                         <li class="nav-item">
@@ -144,7 +166,7 @@ if ($_SESSION['grup_id'] == 4){
 
 <nav id="menu_lateral" class="position-fixed sidenav navbar navbar-light nav_bar_empreendimento" >
 
-<ul class="navbar-nav mr-auto" style="margin-left:10px;">
+<ul class="navbar-nav nav-pills" style="margin-left:0px;">
 
     <?php echo $menu ?>
 </ul>
@@ -156,7 +178,7 @@ if ($_SESSION['grup_id'] == 4){
 </body>
 
 </html> 
-<script>
+<script>/*
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -168,5 +190,5 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
-}
+}*/
 </script>
